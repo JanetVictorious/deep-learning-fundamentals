30a8b8d8f6767fd88c918425bcebe96d17dcfc44
Janet <viktor.t.zetterberg@gmail.com>
2022-11-03 17:01:24 +0100

    Add/Update

--- 01_basic_neural_networks/simple_nn.py
+++ 01_basic_neural_networks/simple_nn.py
@@ -37,7 +37,10 @@ class OneLayerNN:
         return params
 
     def _forward_prop(self, X, params):
-        """Forward propagation in network."""
+        """Forward propagation in network.
+
+        Hidden layer uses `tanh` activation, output layers uses `sigmoid` activation.
+        """
         # Retrieve parameters
         W1 = params['W1']
         b1 = params['b1']
@@ -56,4 +59,129 @@ class OneLayerNN:
                  'Z2': Z2,
                  'A2': A2}
 
-        return cache
+        return A2, cache
+
+    def _cost(self, A2, y):
+        """Compute cost of network."""
+        # Nr of examples
+        m = float(y.shape[1])
+
+        # Cross-entropy cost
+        logprobs = -(y * np.log(A2) + (1 - y) * np.log(1 - A2))
+        cost = np.sum(logprobs) / m
+
+        # Squeeze to right dimension
+        cost = float(np.squeeze(cost))
+
+        return cost
+
+    def _backward_prop(self, params, cache, X, y):
+        """Backward propagation in network."""
+        # Nr of examples
+        m = float(X.shape[1])
+
+        # Retrieve weigths
+        W2 = params['W2']
+
+        # Retrieve forward propagation outputs
+        A1 = cache['A1']
+        A2 = cache['A2']
+
+        # Backward propagation
+        dZ2 = A2 - y
+        dW2 = np.dot(dZ2, A1.T) / m
+        db2 = np.sum(dZ2, axis=1, keepdims=True) / m
+        dZ1 = np.dot(W2.T, dZ2) * (1 - np.power(A1, 2))
+        dW1 = np.dot(dZ1, X.T) / m
+        db1 = np.sum(dZ1, axis=1, keepdims=True) / m
+
+        # Gradients
+        grads = {'dW1': dW1,
+                 'db1': db1,
+                 'dW2': dW2,
+                 'db2': db2}
+
+        return grads
+
+    def _upgrade_params(self, params, grads, learning_rate: float = 0.1):
+        """Update parameters using gradient descent."""
+        # Retrieve weights and biases
+        W1 = np.array([i for i in params['W1']])
+        b1 = params['b1']
+        W2 = np.array([i for i in params['W2']])
+        b2 = params['b2']
+
+        # Retrieve gradients
+        dW1 = grads['dW1']
+        db1 = grads['db1']
+        dW2 = grads['dW2']
+        db2 = grads['db2']
+
+        # Gradient descent
+        W1 = W1 - learning_rate * dW1
+        b1 = b1 - learning_rate * db1
+        W2 = W2 - learning_rate * dW2
+        b2 = b2 - learning_rate * db2
+
+        params = {'W1': W1,
+                  'b1': b1,
+                  'W2': W2,
+                  'b2': b2}
+
+        return params
+
+    def call(self, X, y, n_h: int = 4, num_iter: int = 10000, learning_rate: float = 0.1, print_cost: bool = False):
+        """Train network with 1 hidden layer."""
+        np.random.seed(42)
+
+        # Input/Output layers
+        n_x, n_h, n_y = self._layer_sizes(X, y, hidden_units=n_h)
+
+        # Initialize parameters
+        params = self._init_params(n_x, n_h, n_y)
+        costs = []
+
+        for i in range(num_iter):
+            # Forward propagation
+            A2, cache = self._forward_prop(X, params)
+
+            # Cost
+            cost = self._cost(A2, y)
+            if i % 100 == 0:
+                costs.append(cost)
+            if print_cost and i % 1000 == 0:
+                print(f'Cost after iteration {i}: {cost}')
+
+            # Backward propagation
+            grads = self._backward_prop(params, cache, X, y)
+
+            # Gradient descent
+            params = self._upgrade_params(params, grads, learning_rate)
+
+        # Save learned parameters
+        self.params = params
+
+        return self
+
+    def _predict(self, X):
+        """Use learned parameters for prediction."""
+        # Verify learned parameters exist
+        if self.params == dict():
+            err_msg = 'No learned parameters. Train model using `.call()` method.'
+            raise ValueError(err_msg)
+
+        # Forward prop with learned parameters
+        A2, _ = self._forward_prop(X, self.params)
+
+        return A2
+
+    def predict(self, X):
+        """Binary prediction."""
+        y_pred = self._predict(X)
+        y_pred = (y_pred > 0.5)
+        y_pred = np.array(y_pred).reshape(-1, 1)
+        return y_pred
+
+    def predict_proba(self, X):
+        """Predict probabilities."""
+        return self._predict(X)
---
+++ 01_basic_neural_networks/utils/data_utils.py
@@ -0,0 +1,51 @@
+import numpy as np
+from sklearn.datasets import make_blobs
+import matplotlib.pyplot as plt
+
+
+def cluster_dataset():
+    """Generate four clusters for classification."""
+    np.random.seed(1)
+    m = 800  # Number of examples
+    N = int(m / 4)  # Number of points per class
+    D = 2  # Number of features
+    X = np.zeros((m, D))  # Feature matrix
+    y = np.zeros((m, 1), dtype='uint8')  # Label array
+
+    # Generate clusters
+    centers = [[-1, -1], [-1, 1], [1, 1], [1, -1]]
+    for j in range(4):
+        ix = range(N * j, N * (j + 1))
+        x, _ = make_blobs(n_samples=N, centers=[centers[j]], cluster_std=0.6)
+        X[ix] = x
+        y[ix] = j % 2
+
+    X = X.T
+    y = y.T
+
+    return X, y
+
+
+def plot_decision_boundary(model, X, y):
+    # Set min and max values and give it some padding
+    x_min, x_max = X[0, :].min() - 1, X[0, :].max() + 1
+    y_min, y_max = X[1, :].min() - 1, X[1, :].max() + 1
+    h = 0.01
+    
+    # Generate a grid of points with distance h between them
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+    
+    # Predict the function value for the whole grid
+    Z = model(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    
+    # Plot the contour and training examples
+    plt.figure(figsize=(7, 5))
+    plt.contourf(xx, yy, Z)
+    plt.ylabel('x2')
+    plt.xlabel('x1')
+    plt.scatter(X[0, :], X[1, :], c=y)
+
+
+if __name__ == '__main__':
+    cluster_dataset()
