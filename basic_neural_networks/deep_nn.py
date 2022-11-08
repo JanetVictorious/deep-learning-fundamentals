b5eedf6628edfea2648f9f3eb95e8fd9bab1553e
Janet <viktor.t.zetterberg@gmail.com>
2022-11-08 13:53:52 +0100

    Update

--- basic_neural_networks/deep_nn.py
+++ basic_neural_networks/deep_nn.py
@@ -1,20 +1,18 @@
 import numpy as np
 
-from utils.dnn_utils import sigmoid, relu, sigmoid_backward, relu_backward
+from utils.dnn_utils import sigmoid, relu, tanh, sigmoid_backward, relu_backward, tanh_backward
 
 
 class DeepNNModel:
     def __init__(self):
         self.layer_dims = []
+        self.hidden_activation = None
         self.params = dict()
         self.learning_curve = []
 
     def _init_params(self, layer_dims):
-        """Initialize parameters for deep network."""
-        np.random.seed(42)
+        """Initialize parameters."""
         params = dict()
-
-        # Layers of network
         L = len(layer_dims)
 
         for i in range(1, L):
@@ -24,68 +22,64 @@ class DeepNNModel:
             assert params['W' + str(i)].shape == (layer_dims[i], layer_dims[i - 1])
             assert params['b' + str(i)].shape == (layer_dims[i], 1)
 
-        self.layer_dims = layer_dims
-
         return params
 
     def _linear_forward(self, A, W, b):
-        """Linear part of forward propagation."""
         Z = np.dot(W, A) + b
-        cache = (A, W, b)
-
-        return Z, cache
+        linear_cache = (A, W, b)
+        return Z, linear_cache
 
     def _activation_forward(self, A_prev, W, b, activation):
-        """Linear activation forward."""
-        # Linear forward
         Z, linear_cache = self._linear_forward(A_prev, W, b)
-
-        # Activation
         if activation == 'sigmoid':
             A, activation_cache = sigmoid(Z)
+        elif activation == 'tanh':
+            A, activation_cache = tanh(Z)
         elif activation == 'relu':
             A, activation_cache = relu(Z)
         else:
-            err_msg = f'Activation {activation} not implemented.'
+            err_msg = f'Activation {activation} is not implemented.'
             raise ValueError(err_msg)
 
-        # Cache linear and activation
-        # Contains: A_prev, W, b, and Z
         cache = (linear_cache, activation_cache)
-
         return A, cache
 
-    def _forward_prop(self, X, params):
-        """Forward propagation in network."""
-        caches = []
+    def _forward_prop(self, X, params, hidden_activation):
+        """Forward propagation in network"""
+        # Input layer as first activation
         A = X
+
+        # Length of layers in network
         L = len(params) // 2
 
-        # Forward propagation in hidden layers
+        # List for storing caches for each layer
+        caches = []
+
+        # Forward propagation over all hidden layers
         for i in range(1, L):
             A_prev = A
-            A, cache = self._activation_forward(A_prev,
-                                                params['W' + str(i)],
-                                                params['b' + str(i)],
-                                                activation='relu')
+            A, cache = self._activation_forward(A_prev, params['W' + str(i)], params['b' + str(i)], hidden_activation)
             caches.append(cache)
 
-        # Forward propagation in output layer
-        AL, cache = self._activation_forward(A,
-                                             params['W' + str(L)],
-                                             params['b' + str(L)],
-                                             activation='sigmoid')
+        # Forward propagation of output layer
+        AL, cache = self._activation_forward(A, params['W' + str(L)], params['b' + str(L)], 'sigmoid')
         caches.append(cache)
 
         return AL, caches
 
-    def _cost(self, A, y):
-        """Cost of network."""
+    def _cost(self, AL, y):
+        """Compute cost of network."""
         # Nr of examples
         m = float(y.shape[1])
 
+        # Handle probabilities which are 0 or 1
+        # This is to avoid getting -Inf, or Inf
+        epsilon = 1e-15
+        AL[AL == 1.0] = 1.0 - epsilon
+        AL[AL == 0.0] = epsilon
+
         # Cross-entropy cost
-        logprobs = -(y * np.log(A) + (1 - y) * np.log(1 - A))
+        logprobs = -(y * np.log(AL) + (1.0 - y) * np.log(1.0 - AL))
         cost = np.sum(logprobs) / m
 
         # Squeeze to right dimension
@@ -93,113 +87,134 @@ class DeepNNModel:
 
         return cost
 
-    def _linear_backward(self, dZ, cache):
-        """Linear part of backward propagation."""
-        # Cached properties
-        A_prev, W, b = cache
-
-        # Nr of examples
+    def _linear_backward(self, dZ, linear_cache):
+        A_prev, W, b = linear_cache
         m = float(A_prev.shape[1])
-
-        # Linear backward
         dW = np.dot(dZ, A_prev.T) / m
         db = np.sum(dZ, axis=1, keepdims=True) / m
         dA_prev = np.dot(W.T, dZ)
 
         return dA_prev, dW, db
 
-    def _activation_backward(self, da, cache, activation):
-        """Linear activation backward."""
-        # Linear cache (A_prev, W, b) and activation cache (Z)
+    def _activation_backward(self, dA, cache, activation):
         linear_cache, activation_cache = cache
-
-        # Backward activation
         if activation == 'sigmoid':
-            dz = sigmoid_backward(da, activation_cache)
+            dZ = sigmoid_backward(dA, activation_cache)
+        elif activation == 'tanh':
+            dZ = tanh_backward(dA, activation_cache)
         elif activation == 'relu':
-            dz = relu_backward(da, activation_cache)
+            dZ = relu_backward(dA, activation_cache)
         else:
             err_msg = f'Activation {activation} is not implemented.'
             raise ValueError(err_msg)
 
-        # Linear backward
-        da_prev, dw, db = self._linear_backward(dz, linear_cache)
+        dA_prev, dW, db = self._linear_backward(dZ, linear_cache)
 
-        return da_prev, dw, db
+        return dA_prev, dW, db
 
-    def _backward_prop(self, AL, y, caches):
+    def _backward_prop(self, AL, y, caches, hidden_activation):
         """Backward propagation in network."""
-        # Dictionary for storing gradients
         grads = dict()
         L = len(caches)
         y = y.reshape(AL.shape)
 
-        dAL = -(np.divide(y, AL) - np.divide(1 - y, 1 - AL))
+        # Derivative of cost function w.r.t activation
+        dAL = AL - y
+        assert dAL.shape == AL.shape
 
-        current_cache = caches[L - 1]
-        da_prev_temp, dw_temp, db_temp = self._activation_backward(dAL, current_cache, 'sigmoid')
-        grads['dA' + str(L - 1)] = da_prev_temp
-        grads['dW' + str(L)] = dw_temp
-        grads['db' + str(L)] = db_temp
+        # Derivatives from output layer
+        cache = caches[L - 1]
+        dA_prev, dW, db = self._activation_backward(dAL, cache, 'sigmoid')
+        grads['dA' + str(L - 1)] = dA_prev
+        grads['dW' + str(L)] = dW
+        grads['db' + str(L)] = db
 
-        for i in reversed(range(L - 1)):
-            current_cache = caches[i]
-            da_prev_temp, dw_temp, db_temp = self._activation_backward(da_prev_temp, current_cache, 'relu')
-            grads['dA' + str(i)] = da_prev_temp
-            grads['dW' + str(i + 1)] = dw_temp
-            grads['db' + str(i + 1)] = db_temp
+        # Derivatives of hidden layers
+        for i in reversed(range(1, L)):
+            cache = caches[i - 1]
+            dA_prev, dW, db = self._activation_backward(dA_prev, cache, hidden_activation)
+            grads['dA' + str(i - 1)] = dA_prev
+            grads['dW' + str(i)] = dW
+            grads['db' + str(i)] = db
 
         return grads
 
-    def _upgrade_params(self, params, grads, learning_rate: float = 0.1):
-        """Upgrade parameters using gradient descent."""
-        parameters = {i: v for i, v in params.items()}
+    def _upgrade_params(self, params, grads, learning_rate):
+        """Update parameters using gradient descent."""
+        # Copy parameters with weights and biases
+        parameters = {key: value for key, value in params.items()}
+
+        # Nr of layers to iterate over
         L = len(parameters) // 2
 
-        for i in range(L):
-            parameters['W' + str(i + 1)] = parameters['W' + str(i + 1)] - learning_rate * grads['dW' + str(i + 1)]
-            parameters['b' + str(i + 1)] = parameters['b' + str(i + 1)] - learning_rate * grads['db' + str(i + 1)]
+        for i in range(1, L + 1):
+            parameters['W' + str(i)] = parameters['W' + str(i)] - learning_rate * grads['dW' + str(i)]
+            parameters['b' + str(i)] = parameters['b' + str(i)] - learning_rate * grads['db' + str(i)]
 
         return parameters
 
-    def call(self, X, y, layer_dims, num_iter: int = 10000, learning_rate: float = 0.1, print_cost: bool = False):
-        """Train deep neural network."""
+    def call(self, X, y, layer_dims, hidden_activation='relu', num_iter: int = 10000,
+             learning_rate: float = 0.1, print_cost: bool = False):
+        """Train network."""
         np.random.seed(42)
 
+        self.hidden_activation = hidden_activation
+
+        # Initialize parameters
         params = self._init_params(layer_dims)
         costs = []
 
         for i in range(num_iter):
-            AL, caches = self._forward_prop(X, params)
+            # Forward propagation
+            AL, caches = self._forward_prop(X, params, hidden_activation)
 
+            # Cost
             cost = self._cost(AL, y)
-            if i % 100 == 0:
+            if i % 100 == 0 or i == num_iter - 1:
                 costs.append([i, cost])
-            if print_cost and i % 1000 == 0:
-                print(f'Cost after iteration {i}: {cost}')
+                if print_cost:
+                    print(f'Cost after iteration {i}: {cost}')
 
-            grads = self._backward_prop(AL, y, caches)
+            # Backward propagation
+            grads = self._backward_prop(AL, y, caches, hidden_activation)
 
+            # Gradient descent
             params = self._upgrade_params(params, grads, learning_rate)
 
+        # Save learned parameters
         self.params = params
 
+        # Save learning curve
         self.learning_curve = np.array(costs).reshape(-1, 2)
 
         return self
 
     def _predict(self, X):
-        """Use learned parameters for prediction."""
+        """Use learned parameters for prediction.
+
+        :param X:
+            Input features array.
+        :returns:
+            Array with probabilities from last activation.
+        """
+        # Verify learned parameters exist
         if self.params == dict():
-            err_msg = 'No learned parameters. Train model using `.cal()` method.'
+            err_msg = 'No learned parameters. Train model using `.call()` method.'
             raise ValueError(err_msg)
 
-        AL, _ = self._forward_prop(X, self.params)
+        # Forward prop with learned parameters
+        A2, _ = self._forward_prop(X, self.params, self.hidden_activation)
 
-        return AL
+        return A2
 
     def predict(self, X):
-        """Binary prediction."""
+        """Binary prediction.
+
+        :param X:
+            Input features array.
+        :returns:
+            Binary array from last activation.
+        """
         y_pred = self._predict(X)
         y_pred = (y_pred > 0.5)
         y_pred = np.array(y_pred, dtype=int)
@@ -217,7 +232,7 @@ if __name__ == '__main__':
     from sklearn.metrics import log_loss, f1_score, accuracy_score
 
     # Generate synthetic data
-    X, y = dt.make_classification(n_samples=2000, n_features=200, n_informative=100, n_redundant=100, random_state=42)
+    X, y = dt.make_classification(n_samples=2000, n_features=4, n_informative=2, n_redundant=1, random_state=42)
 
     # Train/Test split
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
@@ -230,10 +245,11 @@ if __name__ == '__main__':
 
     # Instantiate model
     model = DeepNNModel()
-    layer_dims = [X_train.shape[0], 20, 7, 5, y_train.shape[0]]  # 4-layer model
+    layer_dims = [X_train.shape[0], 4, y_train.shape[0]]
 
     # Fit model
-    model.call(X_train, y_train, layer_dims, learning_rate=0.01, print_cost=True)
+    model.call(X_train, y_train, layer_dims, hidden_activation='relu',
+               num_iter=2500, learning_rate=0.1, print_cost=True)
 
     # Predict on test data
     y_pred = model.predict(X_test)
