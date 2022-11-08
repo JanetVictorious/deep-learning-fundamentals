da055f7085aefa48a4430a9c2461b1b2fb75b6a0
Janet <viktor.t.zetterberg@gmail.com>
2022-11-08 13:52:53 +0100

    Add tanh activation

--- basic_neural_networks/utils/dnn_utils.py
+++ basic_neural_networks/utils/dnn_utils.py
@@ -4,31 +4,47 @@ import numpy as np
 def sigmoid(z):
     """Sigmoid activation function."""
     a = 1 / (1 + np.exp(-z))
-    cache = z
-    return a, cache
+    assert a.shape == z.shape
+    activation_cache = z
+    return a, activation_cache
+
+
+def tanh(z):
+    """Tanh activation function."""
+    a = np.tanh(z)
+    assert a.shape == z.shape
+    activation_cache = z
+    return a, activation_cache
 
 
 def relu(z):
     """ReLU activation function."""
     a = np.maximum(0, z)
     assert a.shape == z.shape
-    cache = z
-    return a, cache
+    activation_cache = z
+    return a, activation_cache
 
 
-def sigmoid_backward(da, cache):
+def sigmoid_backward(da, activation_cache):
     """Backward propagation of sigmoid unit."""
-    z = cache
+    z = activation_cache
     s = 1 / (1 + np.exp(-z))
     dz = da * s * (1 - s)
     assert dz.shape == z.shape
     return dz
 
 
-def relu_backward(da, cache):
+def tanh_backward(da, activation_cache):
+    """Backward propagation of tanh unit."""
+    z = activation_cache
+    s = np.tanh(z)
+    dz = da * (1 - np.power(s, 2))
+    return dz
+
+
+def relu_backward(da, activation_cache):
     """Backward propagation of ReLU unit."""
-    z = cache
+    z = activation_cache
     dz = np.array(da, copy=True)
     dz[dz <= 0] = 0
-    assert dz.shape == z.shape
     return dz
