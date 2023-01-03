2feabc64c4fe30aff6140819e0e948d4a9a8a94a
Viggo <viktor.t.zetterberg@gmail.com>
2023-01-03 07:31:49 +0100

    Modify

--- basic_neural_networks/tf_model.py
+++ basic_neural_networks/tf_model.py
@@ -1,10 +1,12 @@
 import tensorflow as tf
+import numpy as np
 
 
-def normalize_image(image):
+def normalize_img(image, label):
+    """Transform image and normalize."""
     image = tf.cast(image, tf.float32) / 255.0
     image = tf.reshape(image, [-1, ])
-    return image
+    return image, label
 
 
 class SimpleTfModel:
@@ -13,15 +15,15 @@ class SimpleTfModel:
         self.train_acc = []
         self.test_acc = []
 
-    def _init_params(self):
+    def _init_params(self, input_shape=12288, output_shape=6):
         """Initialize parameters for network."""
         initializer = tf.keras.initializers.GlorotNormal(seed=1)
-        W1 = tf.Variable(initializer(shape=(25, 12288)))
+        W1 = tf.Variable(initializer(shape=(25, input_shape)))
         b1 = tf.Variable(initializer(shape=(25, 1)))
         W2 = tf.Variable(initializer(shape=(12, 25)))
         b2 = tf.Variable(initializer(shape=(12, 1)))
-        W3 = tf.Variable(initializer(shape=(6, 12)))
-        b3 = tf.Variable(initializer(shape=(6, 1)))
+        W3 = tf.Variable(initializer(shape=(output_shape, 12)))
+        b3 = tf.Variable(initializer(shape=(output_shape, 1)))
 
         params = {'W1': W1, 'b1': b1,
                   'W2': W2, 'b2': b2,
@@ -54,15 +56,30 @@ class SimpleTfModel:
         cost = tf.reduce_sum(cost)
         return cost
 
-    def call(self, X_train, y_train, X_test, y_test,
+    def call(self, X_train, X_test, y_train=None, y_test=None, num_classes=None,
              learning_rate=0.01, num_epochs=1500, minibatch_size=32, print_cost=False):
         """Train network."""
         costs = []
         train_acc = []
         test_acc = []
 
+        # Nr of training examples
+        if isinstance(X_train, tf.data.Dataset) and not y_train and not y_test:
+            m = X_train.cardinality().numpy()
+            if not num_classes:
+                unique_labels = set()
+                for _, label in X_train:
+                    unique_labels.add(label.numpy())
+                num_classes = len(unique_labels)
+        elif isinstance(X_train, np.ndarray) and y_train and y_test:
+            m = X_train.shape[1]
+            num_classes = len(set([i for i in y_train]))
+        else:
+            err_msg = f'Data is of type {type(X_train)} which is not compatible.'
+            raise ValueError(err_msg)
+
         # Init parameters
-        params = self._init_params()
+        params = self._init_params(input_shape=m, output_shape=num_classes)
 
         # Extract parameters
         W1 = params['W1']
@@ -80,15 +97,16 @@ class SimpleTfModel:
         test_accuracy = tf.keras.metrics.CategoricalAccuracy()
 
         # Datasets
-        ds_train = tf.data.Dataset.zip((X_train, y_train))
-        ds_test = tf.data.Dataset.zip((X_test, y_test))
-
-        # Nr of training examples
-        m = ds_train.cardinality().numpy()
+        if isinstance(X_train, tf.data.Dataset) and not y_train and not y_test:
+            ds_train = X_train
+            ds_test = X_test
+        else:
+            ds_train = tf.data.Dataset.zip((X_train, y_train))
+            ds_test = tf.data.Dataset.zip((X_test, y_test))
 
         # Create mini-batches
-        mini_batches = ds_train.batch(minibatch_size).prefetch(8)
-        mini_batches_test = ds_test.batch(minibatch_size).prefetch(8)
+        mini_batches = ds_train.batch(minibatch_size).prefetch(tf.data.AUTOTUNE)
+        mini_batches_test = ds_test.batch(minibatch_size).prefetch(tf.data.AUTOTUNE)
 
         # Train over epochs
         for epoch in range(num_epochs):
@@ -148,3 +166,35 @@ class SimpleTfModel:
 if __name__ == '__main__':
     """Debugging."""
     import tensorflow_datasets as tfds
+
+    # Load mnist dataset
+    (ds_train, ds_test), ds_info = tfds.load(
+        'mnist',
+        split=['train', 'test'],
+        shuffle_files=True,
+        as_supervised=True,
+        with_info=True,
+    )
+
+    # Print info
+    print(ds_info.splits['train'].num_examples)
+    print(ds_train.element_spec)
+    print(ds_info.features['label'].num_classes)
+
+    # Prep data
+    ds_train = ds_train.map(normalize_img, num_parallel_calls=tf.data.AUTOTUNE)
+    ds_test = ds_test.map(normalize_img, num_parallel_calls=tf.data.AUTOTUNE)
+    print(ds_train.element_spec)
+    print(ds_test.element_spec)
+    ds_train = ds_train.cache()
+    ds_test = ds_test.cache()
+    ds_train = ds_train.shuffle(ds_info.splits['train'].num_examples)
+    ds_test = ds_test.shuffle(ds_info.splits['test'].num_examples)
+    num_classes = ds_info.features['label'].num_classes
+    # ds_train = ds_train.batch(128)
+    # ds_train = ds_train.prefetch(tf.data.AUTOTUNE)
+
+    # Pass through network
+    model = SimpleTfModel()
+    model.call(X_train=ds_train, X_test=ds_test, num_classes=num_classes,
+               learning_rate=0.01, num_epochs=1500, minibatch_size=32, print_cost=False)
