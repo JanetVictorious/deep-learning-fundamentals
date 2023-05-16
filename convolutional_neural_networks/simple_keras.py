bf5526e9c438ea18974ea7751e05611cb7a35372
Viggo <viktor.t.zetterberg@gmail.com>
2023-05-16 22:57:44 +0200

    Add docstrings

--- convolutional_neural_networks/simple_keras.py
+++ convolutional_neural_networks/simple_keras.py
@@ -4,11 +4,26 @@ import tensorflow.keras.layers as tfl
 
 class KerasSeqClassifier:
     def __init__(self, input_shape: tuple):
+        """Initialization of sequential classifier.
+
+        Args:
+            input_shape (tuple): Input shape of data, e.g. (28, 28, 3).
+
+        Returns:
+            None.
+        """
         self.input_shape = input_shape
         self.model = None
 
     def _make_classifier(self) -> tf.keras.Sequential:
-        """Sequential model for multi-class classification model."""
+        """Sequential model for multi-class classification model.
+
+        Args:
+            None.
+
+        Returns:
+            A compiled tf.keras.Sequential model.
+        """
         model = tf.keras.Sequential([
             # ZeroPadding2D with padding 3
             tf.keras.Input(shape=self.input_shape),
@@ -36,7 +51,14 @@ class KerasSeqClassifier:
         return model
 
     def _build_model(self) -> tf.keras.Sequential:
-        """Build model."""
+        """Build model.
+
+        Args:
+            None.
+
+        Returns:
+            A compiled tf.keras.Sequential model.
+        """
         # Instantiate model
         model = self._make_classifier()
 
@@ -50,8 +72,17 @@ class KerasSeqClassifier:
 
         return model
 
-    def call(self, ds_train, ds_eval, epochs) -> None:
-        """Train classifier."""
+    def call(self, ds_train: tf.data.Dataset, ds_eval: tf.data.Dataset, epochs: int) -> None:
+        """Train classifier.
+
+        Args:
+            ds_train (tf.data.Dataset): Train dataset.
+            ds_eval (tf.data.Dataset): Validation dataset.
+            epochs (int): Nr of epochs to train.
+
+        Returns:
+            None
+        """
 
         # Build model
         model = self._build_model()
@@ -62,5 +93,11 @@ class KerasSeqClassifier:
         self.model = model
 
     def evaluate(self, ds_test) -> None:
-        """Evaluate trained model."""
+        """Evaluate trained model.
+
+        Args:
+            ds_test (tf.data.Dataset): Test dataset.
+
+        Returns:
+        """
         self.model.evaluate(ds_test)
